6d3aa9619e5d52721394f1b8d3978af4cf168c3e
phunghoang3b <nguyenhoangkhai492000@gmail.com>
2021-06-17 16:25:03 +0700

    #sưa vài thanh tieu de va làm cái tin tuc

---
+++ DoAnWeb/Content/js/slider.js
@@ -0,0 +1,42 @@
+﻿const glide2 = document.getElementById("glide2");
+const slider5 = document.getElementById("glide_5");
+if (glide2)
+    new Glide(glide2, {
+        type: "carousel",
+        startAt: 0,
+        perView: 5,
+        hoverpause: false,
+        autoplay: 2000,
+        animationDuration: 800,
+        animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+        breakpoints: {
+            1200: {
+                perView: 3,
+            },
+            768: {
+                perView: 2,
+            },
+        },
+    }).mount();
+
+if (slider5) {
+    new Glide("#glide_5", {
+        type: "carousel",
+        startAt: 0,
+        perView: 3,
+        rewin: false,
+        autoplay: 3000,
+        animationDuration: 800,
+        animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+        breakpoints: {
+            998: {
+                perView: 2,
+            },
+            768: {
+                perView: 1,
+            },
+        },
+    }).mount();
+}
+
+
