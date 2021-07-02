bb35ab72e48766fb2a8a8e1c3184760fbc9da64a
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-07-02 11:23:04 +0700

    #backend/dangky

---
+++ DoAnWeb/Controllers/NguoiDungController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAnWeb.Models;
+
+namespace DoAnWeb.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbQLBanGameDataContext db = new dbQLBanGameDataContext();
+
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, tblKhacHang kh)
+        {
+            //Gán các giá trị người dùng nhập liệu cho các biến
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var nhaplaimatkhau = collection["Nhaplaimatkhau"];
+            var email = collection["Email"];
+            var diachi = collection["Diachi"];
+            var dienthoai = collection["Dienthoai"];
+            if (string.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (string.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (string.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Mật khẩu không được để trống";
+            }
+            else if (string.IsNullOrEmpty(nhaplaimatkhau))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (string.IsNullOrEmpty(email))
+            {
+                ViewData["loi5"] = "Phải nhập email";
+            }
+            else if (string.IsNullOrEmpty(diachi))
+            {
+                ViewData["Loi6"] = "Địa chỉ không được bỏ trống";
+            }
+            else if (string.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi7"] = "Phải nhập số điện thoại";
+            }
+            else
+            {
+                //gán giá trị cho đối tượng được tạo mới (kh)
+                kh.TenKH = hoten;
+                kh.Username = tendn;
+                kh.Password = matkhau;
+                kh.Email = email;
+                kh.DiaChi = diachi;
+                kh.SDT = dienthoai;
+                db.tblKhacHangs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("Dangnhap");
+            }
+            return this.Dangky();
+        }
+    }
+}
