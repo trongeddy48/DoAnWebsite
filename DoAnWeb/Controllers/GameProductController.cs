f382b95799e1a429ce95500c20c6bd976ca14498
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-07-04 16:55:29 +0700

    #backend/XongLab07

--- DoAnWeb/Controllers/GameProductController.cs
+++ DoAnWeb/Controllers/GameProductController.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DoAnWeb.Models;
-//using PagedList;
-//using PagedList.Mvc;
+
+using PagedList;
+using PagedList.Mvc;
 
 namespace DoAnWeb.Controllers
 {
@@ -21,13 +22,17 @@ namespace DoAnWeb.Controllers
             return data.tblSanPhams.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
         }
 
-        //public ActionResult Index(int ? page)
-        //{
-        //    //Tao bien sanpham quy dinh tren trang
-        //    int pageSize = 30;
-        //    //Tao bien so trang
-        //    int pageNum = (page ?? 1);
-        //}
+        [HttpGet]
+        public ActionResult Index(int ? page)
+        {
+            //Tạo biến quy định số sản phẩm trên mỗi trang
+            int pageSize = 9;
+            //Tạo biến số trang
+            int pageNum = (page ?? 1);
+            //Lay 5 game moi nhat
+            var gamemoi = Laygamemoi(30);
+            return View(gamemoi.ToPagedList(pageNum, pageSize));
+        }
 
         public ActionResult Index()
         {
